621f115feb3da181cd6b56fdc0e8049e0f8d9fd3
ryansomnia <laskarimmanuel@gmail.com>
2022-11-16 23:21:24 +0700

    login sukses

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
       return (
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Dashboard />}/>
+            <Route path="/" element={<Login />}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/dashboard" element={<Dashboard/>}/>
             <Route path="/datasiswa" element={<DataSiswa/>}/>
--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,17 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import Swal from 'sweetalert2'
+import { useNavigate } from "react-router-dom";
 
+export default function Login() {
   
-  export default function Login() {
-    return (
-    //   <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-    //   <div>
-    //     <div class="text-xl font-medium text-black">ChitChat</div>
-    //     <p class="text-slate-500">You have a new message!</p>
-    //   </div>
-    // </div>
-    <div>
-      
-      <h1>login</h1>
-    </div>
-    )
+  const api = "http://localhost:3014/user/login";
+
+  const [username, setUsername] =useState("");
+  const [password, setPassword] =useState("");
+
+  const navigate = useNavigate(); 
+
+  const saveUser = async(e) =>{
+    e.preventDefault();
+        try {
+         let res =  await axios.post(api, {
+            username,
+            password
+          });
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: `Selamat datang <br> ${res.data.data}`,
+            showConfirmButton: false,
+            timer: 1500
+          })
+          navigate("/dashboard")
+        } catch (err) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Anda memasukan Username atau Password yang salah !'
+          })
+          console.log('err',err.response.status);
+        }
   }
+
+  return (
+    <div className="flex justify-center">
+      <div className="bg-white h-36 w-max">
+        <p className="font-sans text-3xl">Login</p>
+        <form onSubmit={saveUser}>
+          <div className="flex flex-col">
+            <label>Username</label>
+            <input
+              className=" h-8 w-max border-2 rounded "
+              id="username"
+              value={username}
+              onChange={(e)=> setUsername(e.target.value)}
+            ></input>
+            <label>Password</label>
+            <input
+              className=" h-8 w-max border-2 rounded "
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+              id="password"
+              type="password"
+            ></input>
+            <button type="submit" className=" mt-5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              Button
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
