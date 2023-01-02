3dc0ef6e5cbdb48fa3d9adceea5ecd6fed960e85
unknown <you@example.com>
2023-01-02 12:17:30 +0700

    update

--- src/Component/ModaTambahArtikel.js
+++ src/Component/ModaTambahArtikel.js
@@ -51,7 +51,7 @@ const addArticle = async(e) =>{
         type="button" className="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase 
         rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg 
         focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-        data-bs-toggle="modalartikel"
+        data-bs-toggle="modal"
         data-bs-target="#modalartikel">
        <AiFillFileAdd size={30} className="mb-5"/> Tambah Artikel
       </button>
